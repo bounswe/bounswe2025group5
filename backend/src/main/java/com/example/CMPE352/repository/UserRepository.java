be9b64b493ec58c7592e8e71f229469efea82e2d
Cengizhan <cengizbilalsari@gmail.com>
2025-04-13 18:25:09 +0300

    User entity and repo

--- backend/src/main/java/com/example/CMPE352/repository/UserRepository.java
+++ backend/src/main/java/com/example/CMPE352/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.CMPE352.repository;
 
-public interface UserRepository {
+
+import com.example.CMPE352.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
