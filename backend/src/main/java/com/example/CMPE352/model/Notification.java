62f3ee1b20048e38b2259934a1580643d125c43d
Cengizhan <cengizbilalsari@gmail.com>
2025-04-13 18:23:46 +0300

    Notification entity and repo

---
+++ backend/src/main/java/com/example/CMPE352/model/Notification.java
@@ -0,0 +1,32 @@
+package com.example.CMPE352.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Notifications")
+@Data
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+}
