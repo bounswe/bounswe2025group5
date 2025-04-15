62214787eb05338c688bdfa70b86bf9e860af6a4
Cengizhan <cengizbilalsari@gmail.com>
2025-04-15 11:49:28 +0300

    JWT service for creating session token to give client side, the security implementation of this will be made later after meeting

---
+++ backend/src/main/java/com/example/CMPE352/service/JwtService.java
@@ -0,0 +1,43 @@
+package com.example.CMPE352.service;
+
+import com.example.CMPE352.model.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("username", user.getUsername())
+                .claim("isAdmin", user.getIsAdmin())
+                .claim("isModerator", user.getIsModerator())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        String email = extractEmail(token);
+        return email.equals(user.getEmail()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
+    }
+}
