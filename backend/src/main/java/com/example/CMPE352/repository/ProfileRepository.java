9609d9df167a63aa1782bccfc9fa78d4dab41e2e
tanriverdiege <tanriverdiahmetege@gmail.com>
2025-04-19 18:36:15 +0300

    Profile model and repo. modified accordingly for profile related endpoints.

--- backend/src/main/java/com/example/CMPE352/repository/ProfileRepository.java
+++ backend/src/main/java/com/example/CMPE352/repository/ProfileRepository.java
@@ -1,10 +1,15 @@
 package com.example.CMPE352.repository;
 
+import com.example.CMPE352.model.User;
 import com.example.CMPE352.model.Profile;
+
+import com.example.CMPE352.model.response.ProfileResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-    Profile findByUserId(Integer userId);
+    Optional<Profile> findByUser(User user);
 }
