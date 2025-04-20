3071415c9a1916c5af8b5a5f8f0a218c0d16949b
Cengizhan <cengizbilalsari@gmail.com>
2025-04-20 15:20:31 +0300

    adding create profile info endpoint , changing some logic about the request types

--- backend/src/main/java/com/example/CMPE352/controller/ProfileController.java
+++ backend/src/main/java/com/example/CMPE352/controller/ProfileController.java
@@ -1,6 +1,6 @@
 package com.example.CMPE352.controller;
 
-import com.example.CMPE352.model.request.ProfileEditRequest;
+import com.example.CMPE352.model.request.ProfileEditAndCreateRequest;
 import com.example.CMPE352.service.ProfileService;
 import com.example.CMPE352.model.response.ProfileResponse;
 import org.springframework.web.bind.annotation.*;// profile/controller/ProfileController.java
@@ -13,16 +13,20 @@ public class ProfileController {
 
     private final ProfileService service;
 
-    @GetMapping("/{username}/profile")
-    public ProfileResponse getProfileInfo(@PathVariable String username) {
+    @GetMapping("/profile/info")
+    public ProfileResponse getProfileInfo(@RequestParam String username) {
         return service.getProfileInfo(username);
     }
 
-    @PutMapping("/{username}/profile/edit")
-    public ProfileResponse editProfileInfo(
-            @PathVariable String username,
-            @RequestBody ProfileEditRequest newProfileInfo) {
-        return service.editProfileInfo(username, newProfileInfo);
+    @PutMapping("/profile/edit")
+    public ProfileResponse editProfile(
+            @RequestBody ProfileEditAndCreateRequest newProfileInfo) {
+        return service.editProfileInfo(newProfileInfo);
+    }
+    @PostMapping("/profile/create")
+    public ProfileResponse createProfile(
+            @RequestBody ProfileEditAndCreateRequest newProfileInfo) {
+        return service.createProfileInfo(newProfileInfo);
     }
 
 }
