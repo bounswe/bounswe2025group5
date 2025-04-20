81d8543c6ef9987656ba850e0cb2bb4ace142130
Cengizhan <cengizbilalsari@gmail.com>
2025-04-20 21:41:31 +0300

    response and request classes for the waste log and waste goal endpoints

---
+++ backend/src/main/java/com/example/CMPE352/model/request/UpdateWasteLogRequest.java
@@ -0,0 +1,14 @@
+package com.example.CMPE352.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateWasteLogRequest {
+    private Double amount;
+}
