3282622b5e184d689cf1960deb074aa638579cef
Kerim Kasar <kerimkasar@Kerim-MacBook-Air-4.local>
2025-04-21 17:58:29 +0300

    test commit

--- mobile/app/(tabs)/explore.tsx
+++ mobile/app/(tabs)/explore.tsx
@@ -22,7 +22,8 @@ export default function TabTwoScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Explore Page</ThemedText>
       </ThemedView>
-      <ThemedText>This is the page to view different posts from various users.</ThemedText>
+      <ThemedText>This is the for posts.
+      </ThemedText>
   
       
     </ParallaxScrollView>
