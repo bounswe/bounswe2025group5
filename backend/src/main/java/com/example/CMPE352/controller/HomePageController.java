491800e7aeee5387acd5d7aa97b1d5cd3baf1f90
abdarslan <abdarslan.9.8@gmail.com>
2025-05-14 21:52:42 +0300

    fixed energy chart controller

--- backend/src/main/java/com/example/CMPE352/controller/HomePageController.java
+++ backend/src/main/java/com/example/CMPE352/controller/HomePageController.java
@@ -1,16 +1,19 @@
 package com.example.CMPE352.controller;
 
-
 import com.example.CMPE352.model.response.AirQualityResponse;
+import com.example.CMPE352.model.response.EnergyStatResponse;
 import com.example.CMPE352.service.AirQualityService;
 import com.example.CMPE352.model.response.NumberTriviaResponse;
 import com.example.CMPE352.service.MotivationService;
 import com.example.CMPE352.service.NumberService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.CMPE352.model.response.MotivationalQuoteResponse;
-
+import com.example.CMPE352.service.EnergyStatsService;
 
 @RestController
 @RequestMapping("/api/home")
@@ -20,21 +23,24 @@ public class HomePageController {
     private final MotivationService motivationService;
     private final AirQualityService airQualityService;
     private final NumberService numberService;
+    private final EnergyStatsService energyStatsService;    
 
     @GetMapping("/getMotivated")
     public ResponseEntity<MotivationalQuoteResponse> getMotivationalQuote() {
         MotivationalQuoteResponse quote = motivationService.fetchMotivationalQuote();
         return ResponseEntity.ok(quote);
     }
+
     @GetMapping("/number/{number}")
     public ResponseEntity<NumberTriviaResponse> getNumberTrivia(@PathVariable int number) {
         NumberTriviaResponse trivia = numberService.fetchNumberTrivia(number);
         return ResponseEntity.ok(trivia);
     }
 
-
-
-
+    @GetMapping("/energy/{countryCode}")
+    public List<EnergyStatResponse> getEnergyStats(@PathVariable String countryCode) {
+        return energyStatsService.fetchEnergyStats(countryCode);
+    }
 
     @GetMapping("/getAirQuality")
     public AirQualityResponse getAirQuality(@RequestParam String location) {
