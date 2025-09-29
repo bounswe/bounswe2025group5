31f9061249823695da718c2b8f874f28727366f5
CengizBilalSari <136260124+CengizBilalSari@users.noreply.github.com>
2025-09-29 22:06:32 +0300

    correct the create tables

--- archive/database/init.sql
+++ archive/database/init.sql
@@ -1,6 +1,6 @@
-DROP DATABASE zero_waste_challenge_dev; 
-CREATE DATABASE  zero_waste_challenge_dev;
-USE zero_waste_challenge_dev;
+DROP DATABASE IF EXISTS waste_less;
+CREATE DATABASE IF NOT EXISTS waste_less;
+Use  waste_less;
 
 CREATE  TABLE IF NOT EXISTS  `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
@@ -14,7 +14,6 @@ CREATE  TABLE IF NOT EXISTS  `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`)
 ) ;
-
 
 CREATE TABLE IF NOT EXISTS `waste_goal` (
   `goal_id` INT NOT NULL AUTO_INCREMENT,
@@ -30,6 +29,7 @@ CREATE TABLE IF NOT EXISTS `waste_goal` (
   KEY `wg_user` (`user_id`),
   CONSTRAINT `wg_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE IF NOT EXISTS `waste_log` (
   `log_id`   INT NOT NULL AUTO_INCREMENT,
@@ -53,42 +53,11 @@ CREATE TABLE IF NOT EXISTS `waste_log` (
   `waste_type` enum('Glass','Metal','Organic','Paper','Plastic') NOT NULL,
   `amount` double NOT NULL,
   PRIMARY KEY (`challenge_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-
-
-
-CREATE TABLE IF NOT EXISTS  `forumfeeds` (
-  `feed_id` int NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`feed_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
-CREATE TABLE IF NOT EXISTS  `leaderboards` (
-  `leaderboard_id` int NOT NULL AUTO_INCREMENT,
-  `location` varchar(50) DEFAULT NULL,
-  `type` enum('Plastic','Organic','Paper','Metal','Glass') DEFAULT NULL,
-  PRIMARY KEY (`leaderboard_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS  `notifications` (
-  `notification_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `message` varchar(255) NOT NULL,
-  `is_read` tinyint(1) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`notification_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE IF NOT EXISTS  `posts` (
   `post_id` int NOT NULL AUTO_INCREMENT,
-  `feed_id` int DEFAULT NULL,
   `user_id` int NOT NULL,
   `content` varchar(1000) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -96,20 +65,11 @@ CREATE TABLE IF NOT EXISTS  `posts` (
   `comments` int DEFAULT '0',
   `photo_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`post_id`),
-  KEY `feed_id` (`feed_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`feed_id`) REFERENCES `forumfeeds` (`feed_id`) ON DELETE SET NULL,
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS  `postattachments` (
-  `attachment_id` int NOT NULL AUTO_INCREMENT,
-  `post_id` int NOT NULL,
-  `file_url` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`attachment_id`),
-  KEY `post_id` (`post_id`),
-  CONSTRAINT `postattachments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
  CREATE TABLE `user_challenge_progress` (
   `remaining_amount` double DEFAULT NULL,
@@ -120,7 +80,7 @@ CREATE TABLE IF NOT EXISTS  `postattachments` (
   KEY `FKoac9ib3121eou06gwlt094e7q` (`user_id`),
   CONSTRAINT `FKaw0lexcvav1p4svwv76l6o3ga` FOREIGN KEY (`challenge_id`) REFERENCES `challenges` (`challenge_id`),
   CONSTRAINT `FKoac9ib3121eou06gwlt094e7q` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE  IF NOT EXISTS  `profiles` (
@@ -135,13 +95,6 @@ CREATE TABLE  IF NOT EXISTS  `profiles` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE IF NOT EXISTS  `report` (
-  `report_id` int NOT NULL AUTO_INCREMENT,
-  `reason` varchar(500) DEFAULT NULL,
-  `report_date` date NOT NULL,
-  `status` enum('Received','Resolved') DEFAULT NULL,
-  PRIMARY KEY (`report_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS  `rewards` (
   `reward_id` int NOT NULL AUTO_INCREMENT,
@@ -151,6 +104,8 @@ CREATE TABLE IF NOT EXISTS  `rewards` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`reward_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 
 CREATE TABLE IF NOT EXISTS  `user_rewards` (
   `created_at` datetime(6) DEFAULT NULL,
@@ -163,27 +118,7 @@ CREATE TABLE IF NOT EXISTS  `user_rewards` (
   CONSTRAINT `FKj53yk8gtooowu02nlq4isx5dd` FOREIGN KEY (`reward_id`) REFERENCES `rewards` (`reward_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE  IF NOT EXISTS  `userchallenge` (
-  `user_id` int NOT NULL,
-  `challenge_id` int NOT NULL,
-  `joined_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`,`challenge_id`),
-  KEY `challenge_id` (`challenge_id`),
-  CONSTRAINT `userchallenge_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  CONSTRAINT `userchallenge_ibfk_2` FOREIGN KEY (`challenge_id`) REFERENCES `challenges` (`challenge_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-CREATE TABLE IF NOT EXISTS  `userleaderboard` (
-  `leaderboard_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `ranking` int NOT NULL,
-  `score` int NOT NULL,
-  PRIMARY KEY (`leaderboard_id`,`user_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `userleaderboard_ibfk_1` FOREIGN KEY (`leaderboard_id`) REFERENCES `leaderboards` (`leaderboard_id`) ON DELETE CASCADE,
-  CONSTRAINT `userleaderboard_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE  IF NOT EXISTS  `comments` (
@@ -199,21 +134,7 @@ CREATE TABLE  IF NOT EXISTS  `comments` (
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS  `badge` (
-  `badge_id` int NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) NOT NULL,
-  `criteria` varchar(255) NOT NULL,
-  PRIMARY KEY (`badge_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE IF NOT EXISTS  `userhasbadge` (
-  `user_id` int NOT NULL,
-  `badge_id` int NOT NULL,
-  `date_achieved` date NOT NULL,
-  PRIMARY KEY (`user_id`,`badge_id`),
-  KEY `badge_id` (`badge_id`),
-  CONSTRAINT `userhasbadge_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  CONSTRAINT `userhasbadge_ibfk_2` FOREIGN KEY (`badge_id`) REFERENCES `badge` (`badge_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE IF NOT EXISTS `post_likes` (
                                             `user_id` INT NOT NULL,
@@ -236,6 +157,31 @@ CREATE TABLE `saved_posts` (
 ) ENGINE=InnoDB 
   DEFAULT CHARSET=utf8mb4 
   COLLATE=utf8mb4_0900_ai_ci;
+  
+
+  
+CREATE TABLE IF NOT EXISTS  `notifications` (
+  `notification_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `is_read` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`notification_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE badge (
+    name VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (name, user_id),
+    CONSTRAINT fk_badge_user FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
 
 DELIMITER $$
 CREATE TRIGGER `after_like_insert`
