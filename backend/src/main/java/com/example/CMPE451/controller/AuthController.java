dc1d721b8583fe22d74df7e1cc7c1258868ac3b4
Cengizhan <cengizbilalsari@gmail.com>
2025-10-08 10:41:11 +0300

    delete some wrong notations

--- backend/src/main/java/com/example/CMPE451/controller/AuthController.java
+++ backend/src/main/java/com/example/CMPE451/controller/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:19006")
 @RequiredArgsConstructor
 public class AuthController {
 
