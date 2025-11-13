eee8392ddeeba3c9044ac6602a4a6ee9c42ef564
tenacke <emrekilic1010@gmail.com>
2025-11-13 23:10:10 +0300

    fix: revert accidentally edited backend file to prevent later conflicts

--- backend/src/main/java/com/example/CMPE451/Cmpe451Application.java
+++ backend/src/main/java/com/example/CMPE451/Cmpe451Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cmpe451Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Cmpe451Application.class, args);
 	}
+
 }
