b88f49fd962698df3a7485e8077cb5e3b7372c68
yukseleren <erensen5555@gmail.com>
2025-11-18 06:10:13 +0300

    small bug in config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
 import path from "path";
